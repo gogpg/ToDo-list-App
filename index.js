5555af4c7f36c60b10a4ed34f4b5a093b1ea833c
gogpg <giedre.luko@gmail.com>
2022-08-02 15:49:32 +0300

    ToDo-App

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import mysql from 'mysql2';
+
+const app = express();
+const port = 8081;
+app.use(express.static('public'));
+
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars.engine({
+   extname: 'hbs'
+}));
+
+app.get('/test', (req, res) => res.render('test'));
+app.get('/test2', (req, res) => res.render('test2'));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(port, () => console.log(`starting server on port ${port}`));
