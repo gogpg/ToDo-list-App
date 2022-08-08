801a972224cb5fac841e6de15af143fb147b3f23
gogpg <giedre.luko@gmail.com>
2022-08-08 17:18:54 +0300

    update

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import mysql from 'mysql2';
-import mainPage from './lib/mainPage.js';
+import {renderMainPage, insertNewNote, deleteNote, editNote} from './lib/mainPage.js';
 
 const app = express();
 const port = 8081;
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars.engine({
-   extname: 'hbs'
+    extname: 'hbs'
 }));
 
-app.get('/', mainPage);
+app.get('/', renderMainPage);
+app.post('/', insertNewNote);
+app.delete('/', deleteNote);
+app.patch('/', editNote);
 
-//DB start
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'classicmodels',
-    password : 'bit',
-    database : 'classicmodels'
-});
-
-app.get('/db', (req, res) => {
-    connection.execute('SELECT productLine, textDescription FROM productlines', (err, rows) => {
-        const data = rows.map(row => row);
-        res.render('db', {data: data});
-    });
-})
-//DB end
-
-app.listen(port, () => console.log(`starting server on port ${port}`));
+app.listen(port, () => console.log(`Starting server on port ${port}`));
